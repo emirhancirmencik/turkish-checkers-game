8f59ebc8e0d57bf3227da86301297b8442c6ac39
Emirhan ÇİRMENCİK <emrhncrmnck1905@gmail.com>
2022-06-09 02:29:11 +0300

    Board direction fixed.

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 
 function Board() {
   const board = useSelector((state) => state.game.board);
+
   console.log(board);
   return (
     <div className="board">
@@ -10,31 +11,31 @@ function Board() {
         return (
           <div className="col" key={colindex}>
             {col.map((row, rowindex) => {
-              if ((rowindex + colindex) % 2 === 1) {
-                return (
-                  <div
-                    className="square square-even"
-                    key={rowindex}
-                    index={
-                      String.fromCharCode(colindex + 97) + String(8 - rowindex)
-                    }
-                  >
-                    <div className="checker black-checker"></div>
+              return (
+                <div
+                  className={`square ${
+                    (colindex + rowindex) % 2 === 0
+                      ? "square-odd"
+                      : "square-even"
+                  }`}
+                  key={rowindex}
+                  index={
+                    String.fromCharCode(rowindex + 97) + String(8 - colindex)
+                  }
+                >
+                  <div className="checker-container">
+                    <div
+                      className={`${
+                        row === -1
+                          ? "checker black-checker"
+                          : row === 1
+                          ? "checker white-checker"
+                          : ""
+                      } `}
+                    ></div>
                   </div>
-                );
-              } else {
-                return (
-                  <div
-                    className="square square-odd"
-                    key={rowindex}
-                    index={
-                      String.fromCharCode(colindex + 97) + String(8 - rowindex)
-                    }
-                  >
-                    <div className="checker white-checker"></div>
-                  </div>
-                );
-              }
+                </div>
+              );
             })}
           </div>
         );
